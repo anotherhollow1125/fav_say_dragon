af3330631a2179640d32e9334cb1bd4ca6f0a7d2
anotherhollow1125 <anotherhollow1125@gmail.com>
2024-05-12 20:52:59 +0900

    fix: コマンドをサブコマンドに分けた

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use console::{Alignment, Term};
 use serde::Deserialize;
 use std::path::{Path, PathBuf};
@@ -7,42 +7,48 @@ use std::thread::sleep;
 use std::time::Duration;
 
 #[derive(Parser)]
-#[command(version, about)]
+#[command(version, about, flatten_help = true)]
 struct Args {
-    /// おかず
-    side_dishes: Vec<String>,
-
-    /// プレキャプション
-    #[arg(short, long)]
-    pre_captions: Vec<String>,
-
-    /// アフターキャプション
-    #[arg(short = 'A', long)]
-    after_captions: Vec<String>,
-
-    /// ファイルからスクリプトを読み込む
-    #[arg(short = 'f', long,
-        conflicts_with_all(["side_dishes", "pre_captions", "after_captions"]))
-    ]
-    script_file: Option<PathBuf>,
-
-    #[command(flatten)]
-    anime_args: Option<AnimeArgs>,
+    #[command(subcommand)]
+    sub: Command,
 }
 
-#[derive(clap::Args)]
-struct AnimeArgs {
-    /// アニメにするか
-    #[arg(short, long)]
-    anime: bool,
+#[derive(Subcommand, Clone, Debug)]
+enum Command {
+    /// 一度に出力
+    Say {
+        /// おかず
+        side_dish: String,
+        /// キャプション
+        caption: Option<String>,
+    },
+    /// アニメーション出力
+    Anime {
+        /// おかず
+        side_dishes: Vec<String>,
 
-    /// アニメのインターバル
-    #[arg(
-        short, long,
-        default_value_t = 1000,
-        value_parser = clap::value_parser!(u64).range(10..))
-    ]
-    interval: u64,
+        /// プレキャプション
+        #[arg(short, long)]
+        pre_captions: Vec<String>,
+
+        /// アフターキャプション
+        #[arg(short = 'A', long)]
+        after_captions: Vec<String>,
+
+        /// ファイルからスクリプトを読み込む
+        #[arg(short = 'f', long,
+            conflicts_with_all(["side_dishes", "pre_captions", "after_captions"]))
+        ]
+        script_file: Option<PathBuf>,
+
+        /// インターバル (ms)
+        #[arg(
+            short, long,
+            default_value_t = 1000,
+            value_parser = clap::value_parser!(u64).range(10..))
+        ]
+        interval: u64,
+    },
 }
 
 #[derive(Deserialize, Debug)]
@@ -60,14 +66,47 @@ impl Script {
 }
 
 fn main() -> Result<()> {
-    let Args {
-        side_dishes,
-        pre_captions,
-        after_captions,
-        script_file,
-        anime_args,
-    } = Args::parse();
+    match Args::parse().sub {
+        Command::Say { side_dish, caption } => say(&side_dish, caption.as_deref())?,
+        Command::Anime {
+            side_dishes,
+            pre_captions,
+            after_captions,
+            script_file,
+            interval,
+        } => anime(
+            side_dishes,
+            pre_captions,
+            after_captions,
+            script_file,
+            interval,
+        )?,
+    }
 
+    Ok(())
+}
+
+fn say(side_dish: &str, caption: Option<&str>) -> Result<()> {
+    let term = Term::stdout();
+    let terminal_width = term.size().1 as usize;
+
+    let dragon = create_dragon(side_dish, terminal_width);
+    for line in dragon {
+        term.write_line(&line)?;
+    }
+    let caption = console::pad_str(caption.unwrap_or(""), 60, Alignment::Center, None);
+    term.write_line(&caption)?;
+
+    Ok(())
+}
+
+fn anime(
+    side_dishes: Vec<String>,
+    pre_captions: Vec<String>,
+    after_captions: Vec<String>,
+    script_file: Option<PathBuf>,
+    interval: u64,
+) -> Result<()> {
     let (side_dishes, pre_captions, after_captions) = match script_file {
         Some(path) => {
             let Script {
@@ -82,18 +121,7 @@ fn main() -> Result<()> {
 
     let term = Term::stdout();
     let terminal_width = term.size().1 as usize;
-
-    let interval = match anime_args {
-        Some(AnimeArgs {
-            anime: true,
-            interval,
-        }) => {
-            term.clear_screen()?;
-            Some(interval)
-        }
-        _ => None,
-    };
-
+    term.clear_screen()?;
     let empty_dragon = create_dragon("", terminal_width);
     let mut printed_flag = false;
 
@@ -106,19 +134,15 @@ fn main() -> Result<()> {
         term.write_line(&pre_caption)?;
         printed_flag = true;
 
-        if let Some(interval) = interval {
-            if pre_captions_iter.peek().is_some() {
-                clear_dragon(interval, &term, &mut printed_flag)?;
-            }
+        if pre_captions_iter.peek().is_some() {
+            clear_dragon(interval, &term, &mut printed_flag)?;
         }
     }
 
     let mut side_dish_iter = side_dishes.iter().peekable();
 
-    if let Some(interval) = interval {
-        if printed_flag && side_dish_iter.peek().is_some() {
-            clear_dragon(interval, &term, &mut printed_flag)?;
-        }
+    if printed_flag && side_dish_iter.peek().is_some() {
+        clear_dragon(interval, &term, &mut printed_flag)?;
     }
 
     while let Some(side_dish) = side_dish_iter.next() {
@@ -130,19 +154,15 @@ fn main() -> Result<()> {
         term.write_line(&empty_line)?;
         printed_flag = true;
 
-        if let Some(interval) = interval {
-            if side_dish_iter.peek().is_some() {
-                clear_dragon(interval, &term, &mut printed_flag)?;
-            }
+        if side_dish_iter.peek().is_some() {
+            clear_dragon(interval, &term, &mut printed_flag)?;
         }
     }
 
     let mut after_captions_iter = after_captions.into_iter().peekable();
 
-    if let Some(interval) = interval {
-        if printed_flag && after_captions_iter.peek().is_some() {
-            clear_dragon(interval, &term, &mut printed_flag)?;
-        }
+    if printed_flag && after_captions_iter.peek().is_some() {
+        clear_dragon(interval, &term, &mut printed_flag)?;
     }
 
     while let Some(after_caption) = after_captions_iter.next() {
@@ -152,10 +172,8 @@ fn main() -> Result<()> {
         let after_caption = console::pad_str(&after_caption, 60, Alignment::Center, None);
         term.write_line(&after_caption)?;
 
-        if let Some(interval) = interval {
-            if after_captions_iter.peek().is_some() {
-                clear_dragon(interval, &term, &mut printed_flag)?;
-            }
+        if after_captions_iter.peek().is_some() {
+            clear_dragon(interval, &term, &mut printed_flag)?;
         }
     }
 
